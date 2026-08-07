b519ce6de8a7aa4177b93c7ff0740e1610e5ca10
AndrewVj <andsyl@gmail.com>
2026-08-07 02:13:27 -0400

    Assignment Exercise 1.7

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -16,7 +16,9 @@ const Header = ({text}) => {
   )
 }
 
-const Statistics = ({text, value}) => {
+const StatisticsLine = ({text, value}) => {
+  console.log("Text: ", text)
+  console.log("Value: ", value)
   return (
     <div>
       <p>
@@ -26,22 +28,52 @@ const Statistics = ({text, value}) => {
   )
 }
 
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  console.log("The Component Rendered")
+
+  const handleGood = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+    setAverage((good - bad + 1)/(all + 1))
+    setPositive(((good + 1)/(all + 1))*100)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+    setAverage((good - bad)/(all + 1))
+    setPositive((good / (all + 1))*100)
+  }
+  
+  const handleBad = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    setAverage((good - 1 - bad)/(all + 1))
+    setPositive((good / (all + 1))*100)
+  }
 
   return (
     <div>
       <Header text="Give feedback" />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
       <Header text="Statistics" />
-      <Statistics text="good" value={good} />
-      <Statistics text="neutral" value={neutral} />
-      <Statistics text="bad" value={bad} />
+      <StatisticsLine text="good" value={good} />
+      <StatisticsLine text="neutral" value={neutral} />
+      <StatisticsLine text="bad" value={bad} /> 
+      <StatisticsLine text="all" value={all} /> 
+      <StatisticsLine text="average" value={average} /> 
+      <StatisticsLine text="positive" value={`${positive} %`}/>
     </div>
   )
 }
