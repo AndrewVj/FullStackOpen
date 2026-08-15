e9c27f68e2c450c741b7a458492de8305af84dee
AndrewVj <andsyl@gmail.com>
2026-08-15 02:44:11 -0400

    Assignment Exercise 2.11

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from '../components/Persons'
 import Filter from '../components/Filter'
 import PersonForm from '../components/PersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then((response) => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value)
